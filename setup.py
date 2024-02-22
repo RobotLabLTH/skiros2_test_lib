e3da95d9a95e1b991f14d4e5a55e93778791f2a6
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-02-22 10:53:58 +0000

    New: Announce submodule of test lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'skiros2_test_lib'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=[package_name],
+    packages=[package_name, package_name + "/test"],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
